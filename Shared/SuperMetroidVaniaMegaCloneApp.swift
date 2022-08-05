730dc2eac11be82c9f3cab8c78df4af280309ab0
Cuong <s3881006@rmit.edu.vn>
2022-08-05 12:41:11 +0700

    Initial Commit

---
+++ Shared/SuperMetroidVaniaMegaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperMetroidVaniaMegaCloneApp.swift
+//  Shared
+//
+//  Created by Cuong Nguyen Phuc on 05/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SuperMetroidVaniaMegaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
